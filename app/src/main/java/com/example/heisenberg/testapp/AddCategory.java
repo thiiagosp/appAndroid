0962152e302b891a7c2a9f995addbf16862e35a4
Thiago <thiagopereira2634@gmail.com>
2017-11-16 21:04:59 -0200

    tao

--- app/src/main/java/com/example/heisenberg/testapp/AddCategory.java
+++ app/src/main/java/com/example/heisenberg/testapp/AddCategory.java
@@ -1,29 +1,41 @@
 package com.example.heisenberg.testapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
-public class ListSpent extends AppCompatActivity {
+import android.widget.Button;
+
+public class AddCategory extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_spent);
+        setContentView(R.layout.activity_add_category);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+
+
+        Button addCategoryButton=(Button)findViewById(R.id.add_category_button);
+
+        addCategoryButton.setOnClickListener(new Button.OnClickListener(){
+
+            public void onClick(View v)
+            {
+
+            }
+        });
     }
 
-}
+
+
+    }
+
+
