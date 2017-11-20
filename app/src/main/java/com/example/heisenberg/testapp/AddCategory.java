8279fb7c878a4e77ecef9e1da4dc1032275df8c5
Thiago <thiagopereira2634@gmail.com>
2017-11-19 23:25:32 -0200

    gradle permission

--- app/src/main/java/com/example/heisenberg/testapp/AddCategory.java
+++ app/src/main/java/com/example/heisenberg/testapp/AddCategory.java
@@ -1,18 +1,15 @@
 package com.example.heisenberg.testapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-
 import android.widget.Button;
 
 public class AddCategory extends AppCompatActivity {
 
+    private String id;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +27,37 @@ public class AddCategory extends AppCompatActivity {
             public void onClick(View v)
             {
 
+//                HttpClient httpclient = new DefaultHttpClient();
+//                HttpPost httppost = new HttpPost("http://<ip address>:3000");
+//
+//                try {
+//                    //add data
+//                    List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+//                    nameValuePairs.add(new BasicNameValuePair("data", data[0]));
+//                    httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+//                    //execute http post
+//                    HttpResponse response = httpclient.execute(httppost);
+//
+//                } catch (ClientProtocolException e) {
+//
+//                } catch (IOException e) {
+//
+//                }
+
+
             }
         });
     }
 
+}
 
 
-    }
+
+
+
+
+
+
+
 
 
